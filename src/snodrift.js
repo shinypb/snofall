b326851e04ecd4a8596440581517c9ca3a2c1269
Mark Christian <m@rkchristian.ca>
2020-10-11 22:05:14 -0700

    Fairly OK implementation

--- src/snodrift.js
+++ src/snodrift.js
@@ -3,21 +3,44 @@ import "./snodrift.css";
 
 const layers = [];
 
+export function isSnowing() {
+	return layers.length > 0;
+}
+
 function makeLayer() {
 	const el = document.createElement("div");
 	el.className = "snodrift-snow";
 	return el;
 }
 
-export function isSnowing() {
-	return layers.length > 0;
-}
-
-export function start() {
+export function start(
+	{ layerCount, minSpeed, maxSpeed } = {
+		layerCount: 2,
+		minSpeed: 1,
+		maxSpeed: 3,
+	}
+) {
 	if (isSnowing()) return;
 
-	layers.push(makeLayer());
-	layers.forEach((layer) => document.body.appendChild(layer));
+	if (!Number.isInteger(layerCount) || layerCount < 1)
+		throw new Error("Invalid number of layers; must be a positive number");
+
+	for (let i = 0; i < layerCount; i++) {
+		const layer = makeLayer();
+		layer.style.animationDuration = `${
+			10 * Math.floor((minSpeed + Math.random() * (maxSpeed - minSpeed)) * 1000)
+		}ms`;
+
+		if (i % 2 === 0) {
+			layer.className = "snodrift-snow snodrift-snow-1";
+		} else {
+			layer.className = "snodrift-snow snodrift-snow-2";
+		}
+		layer.style.opacity = 0.8 + 0.2 * Math.random();
+
+		layers.push(layer);
+		document.body.appendChild(layer);
+	}
 }
 
 export function stop() {
